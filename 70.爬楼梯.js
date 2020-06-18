40618cd479e3d39a121ba061785a3b9e8d1b8d3d
mingyong-g <mingyong.g@foxmail.com>
2020-06-18 23:53:00 +0800

    [69] 平方根 ； [70] 爬楼梯

---
+++ 70.爬楼梯.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=javascript
+ *
+ * [70] 爬楼梯
+ * 
+ * @desc
+ * - 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *　
+ * - 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * - 注意：给定 n 是一个正整数。
+ * 
+ * @demo 
+ * 输入： 2 输出： 2
+ * - 解释： 有两种方法可以爬到楼顶。
+ * - 1.  1 阶 + 1 阶
+ * - 2.  2 阶
+ * 
+ * 输入： 3  输出： 3
+ * - 解释： 有三种方法可以爬到楼顶。
+ * - 1.  1 阶 + 1 阶 + 1 阶
+ * - 2.  1 阶 + 2 阶
+ * - 3.  2 阶 + 1 阶
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  /** 第一种解放运算超时 */
+  // let base = [1,2];
+  // if(base[n-1]) return base[n-1];
+  // else{
+  //  return  base[n-1] = climbStairs(n-1)+climbStairs(n-2);
+  // }
+
+  /** 第二种解法
+   * @link https://leetcode-cn.com/problems/climbing-stairs/solution/hua-jie-suan-fa-70-pa-lou-ti-by-guanpengchn/
+   */
+  const dp = [];
+  dp[0] = 1;
+  dp[1] = 1;
+  for(let i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
+// @lc code=end
+
