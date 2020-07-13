097f10d0073473941eac0dafd6737d7b1ad94b75
mingyong.g <mingyong.g@foxmail.com>
2020-07-13 23:20:54 +0800

    [121] 买卖股票的最佳时机

---
+++ 121.买卖股票的最佳时机.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=javascript
+ * 
+ * @des 
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+ * 注意：你不能在买入股票前卖出股票。
+ * 
+ * @demo
+ * - 示例1
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ * 
+ * - 示例2
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * 
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = 0;
+  prices.forEach(item=>{
+    if(item<min) min = item;
+    /** 保留所有计算结果中最大的值 */
+    else max = Math.max(max,item-min);
+  })
+  return max;
+};
+// @lc code=end
+
